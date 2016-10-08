ee43fdd671ed65b9436e8d01e77d44faf342fae5
Wannes Gennar <wannes.gennar@gmail.com>
2016-10-08 15:44:13 +0200

    add kernel contract

---
+++ src/Contracts/Kernel.php
@@ -0,0 +1,29 @@
+<?php
+// Created by dealloc. All rights reserved.
+
+namespace Experus\Sockets\Contracts;
+
+use Symfony\Component\Console\Input\InputInterface as Input;
+use Symfony\Component\Console\Output\OutputInterface as Output;
+
+/**
+ * Interface Kernel describes the contract for a Websocket server.
+ *
+ * @package Experus\Sockets\Contracts
+ */
+interface Kernel
+{
+    /**
+     * Initialize the socket runtime.
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return \Experus\Sockets\Contracts\Kernel
+     */
+    public function init(Input $input, Output $output);
+
+    /**
+     * Start the socket server and accept incoming connections.
+     */
+    public function listen();
+}
