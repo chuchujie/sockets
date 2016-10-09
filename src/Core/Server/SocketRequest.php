68e362851e6047499ecdf1e0896b8bec616fc886
Wannes Gennar <wannes.gennar@gmail.com>
2016-10-09 16:53:11 +0200

    extended SocketRequest

--- src/Core/Server/SocketRequest.php
+++ src/Core/Server/SocketRequest.php
@@ -51,4 +51,35 @@ class SocketRequest
     {
         return $this->connection->protocol();
     }
+
+    /**
+     * Retrieve the body as an object or array.
+     *
+     * @param bool $assoc if true the result will be returned as an array, if false this functions returns an object.
+     * @return array|object|null
+     */
+    public function json($assoc = false)
+    {
+        return json_decode($this->message, $assoc);
+    }
+
+    /**
+     * Check if the body is valid JSON.
+     *
+     * @return bool
+     */
+    public function isJson()
+    {
+        return (strpos($this->message, '{', 0) === 0 || strpos($this->message, '[', 0) === 0);
+    }
+
+    /**
+     * Return the raw body.
+     *
+     * @return string
+     */
+    public function body()
+    {
+        return $this->message;
+    }
 }
