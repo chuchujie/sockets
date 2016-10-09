aec1299064c81ea9ed2b1a73be824a9638a45ba5
Wannes Gennar <wannes.gennar@gmail.com>
2016-10-10 00:32:01 +0200

    added trait for middlewares

---
+++ src/Core/Middlewares/Dispatcher.php
@@ -0,0 +1,25 @@
+<?php
+// Created by dealloc. All rights reserved.
+
+namespace Experus\Sockets\Core\Middlewares;
+
+use Experus\Sockets\Core\Server\SocketRequest;
+
+/**
+ * Class Dispatcher provides logic for running a request through a middleware stack.
+ * @package Experus\Sockets\Core\Middlewares
+ */
+trait Dispatcher
+{
+    /**
+     * Run the request through the provided middlewares.
+     *
+     * @param array $middlewares
+     * @param SocketRequest $request
+     * @return array|object|null
+     */
+    public function runThrough(array $middlewares, SocketRequest $request)
+    {
+        // TODO
+    }
+}
