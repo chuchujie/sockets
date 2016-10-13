4a414e1e8f2e889a7975b534d8a3552d874c14d4
Wannes Gennar <wannes.gennar@gmail.com>
2016-10-13 23:52:56 +0200

    documented mocks

--- tests/Mocks/BarController.php
+++ tests/Mocks/BarController.php
@@ -3,10 +3,24 @@
 
 namespace Mocks;
 
+/**
+ * Class BarController is a mock for a controller with a namespace.
+ * @package Mocks
+ */
 class BarController
 {
+    /**
+     * The response unique to this class.
+     *
+     * @var string
+     */
     const DUMMY_RESPONSE = '__BAR_CONTROLLER__BAR__';
 
+    /**
+     * simple method that returns a unique but verifiable string.
+     *
+     * @return string
+     */
     public function bar()
     {
         return self::DUMMY_RESPONSE;
--- tests/Mocks/FooController.php
+++ tests/Mocks/FooController.php
@@ -1,10 +1,23 @@
 <?php
 // Created by dealloc. All rights reserved.
 
+/**
+ * Class FooController is a mock for a controller without a namespace.
+ */
 class FooController
 {
+    /**
+     * The response unique to this class.
+     *
+     * @var string
+     */
     const DUMMY_RESPONSE = '__FOOCONTROLLER__FOO__RESPONSE__';
 
+    /**
+     * simple method that returns a unique but verifiable string.
+     *
+     * @return string
+     */
     public function foo()
     {
         return self::DUMMY_RESPONSE;
