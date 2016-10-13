7dcfd475db75074d8184034bab0fc088004c1efd
Wannes Gennar <wannes.gennar@gmail.com>
2016-10-13 08:35:36 +0200

    setup phpunit

---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+// Created by dealloc. All rights reserved.
+
+use Mockery as m;
+
+class TestCase extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+}
