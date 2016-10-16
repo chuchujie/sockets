c03164a649abd5bf4bd59f006ed4913e44c3f738
Wannes Gennar <wannes.gennar@gmail.com>
2016-10-16 23:32:42 +0200

    fix broken tests

--- tests/SocketClientTest.php
+++ tests/SocketClientTest.php
@@ -3,6 +3,7 @@
 // Created by dealloc. All rights reserved.
 
 use Experus\Sockets\Core\Client\SocketClient;
+use Guzzle\Http\Message\RequestInterface;
 use Mockery as m;
 use Ratchet\WebSocket\Version\RFC6455\Connection;
 
@@ -19,11 +20,23 @@ class SocketClientTest extends TestCase
     protected $socket;
 
     /**
+     * The mocked internal request we're testing with.
+     *
+     * @var RequestInterface|m\MockInterface
+     */
+    protected $request;
+
+    /**
      * Set up the testing environment.
      */
     public function setUp()
     {
         $this->socket = m::mock('\Ratchet\WebSocket\Version\RFC6455\Connection');
+
+        $websocket = new stdClass;
+        $this->request = m::mock(RequestInterface::class);
+        $websocket->request = $this->request;
+        $this->setMagicProperty($this->socket, 'WebSocket', $websocket);
     }
 
     /**
@@ -63,16 +76,25 @@ class SocketClientTest extends TestCase
     }
 
     /**
-     * Test if the socket generates a unique identifier for each connection.
+     * Test if the socket retrieves the websocket key.
      *
      * @test
      */
-    public function generatesUUID()
+    public function retrievesUUID()
     {
         $client = new SocketClient($this->socket);
-        $client2 = new SocketClient($this->socket);
 
-        self::assertNotEquals($client->getUuid(), $client2->getUuid(), 'Socket clients should all have a unique ID');
+        $this->request->shouldReceive('hasHeader')
+            ->with(SocketClient::UUID_HEADER)
+            ->andReturn(true)
+            ->once();
+
+        $this->request->shouldReceive('getHeader')
+            ->with(SocketClient::UUID_HEADER)
+            ->andReturn('uuid')
+            ->once();
+
+        self::assertEquals($client->getUuid(), 'uuid');
     }
 
     /**
@@ -126,18 +148,14 @@ class SocketClientTest extends TestCase
      */
     public function withProtocol()
     {
-        $websocket = new stdClass;
-        $websocket->request = m::mock('\Guzzle\Http\Message\RequestInterface');
-        $websocket->request->shouldReceive('hasHeader')
+        $this->request->shouldReceive('hasHeader')
             ->with(SocketClient::PROTOCOL_HEADER)
             ->andReturn(true)
             ->once();
-        $websocket->request->shouldReceive('getHeader')
+        $this->request->shouldReceive('getHeader')
             ->with(SocketClient::PROTOCOL_HEADER)
             ->andReturn('FOO')
             ->once();
-
-        $this->setMagicProperty($this->socket, 'WebSocket', $websocket);
 
         $client = new SocketClient($this->socket);
 
